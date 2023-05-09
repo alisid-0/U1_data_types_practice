e6c659848c932e99d37564857211f79ce8856944
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-05-09 13:23:45 -0500

    added const myFavoriteSong

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
  * Declare a variable called myFavoriteSong on one line and console.log the value
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
-
+const myFavoriteSong = `2SEATER`
+console.log(`My favorite song is ${myFavoriteSong}.`)
 /*
  * Prompt 2:
  *
